350f906a5073b18721728f9fe8d04f00971a96f5
GlinTob <jodatomx@hotmail.com>
2026-08-13 08:00:53 -0500

    Fix worker path: change from ../audio-processor.worker.js to ./audio-processor-worker.js

--- modules/audio-controller.js
+++ modules/audio-controller.js
@@ -1,7 +1,7 @@
 // modules/audio-controller.js - PROCESADOR ACÚSTICO COMPARTIDO Y ENCODER WAV PCM
 
 export class AudioProcessorController {
-  constructor(workerPath = new URL('../audio-processor.worker.js', import.meta.url)) {
+  constructor(workerPath = new URL('./audio-processor-worker.js', import.meta.url)) {
     this.worker = new Worker(workerPath);
     this.pendingRequests = new Map();
     this.requestId = 0;
