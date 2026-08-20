0022ba5553f5a44562219b03ab73523bd774302f
GlinTob <jodatomx@hotmail.com>
2026-08-19 19:37:42 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- modules/karaoke.js
+++ modules/karaoke.js
@@ -2,6 +2,8 @@
 // Incluye: Dibujo del canvas, detección de pitch, grabación y sincronización con el teleprompter.
 
 import { $ } from '../script.js'; // Asegúrate de que esta ruta sea correcta en tu proyecto
+import { pitch-shifter-processor } from './pitch-shifter-processor.js';
+
 
 // --- Estado Global del Monitor ---
 let karaokeDuoSplitMode = false; // Estado del modo dúo (true = dividido, false = single)
