919eaca2af4d17f63b4ef91a74ed314f7ad6e34b
GlinTob <jodatomx@hotmail.com>
2026-08-20 01:22:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  * CORE ORQUESTRADOR PRINCIPAL — vocalApp Brain (script.js)
  * Manejo de Enrutamiento Asíncrono (Lazy Loading) y Eventos Globales de Interfaz
  */
-import { drawKaraokeMonitor } from '.module/karaoke.js'; 
-import { toggleKaraokeDuoSplitMode } from '.module/karaoke.js';
+import { drawKaraokeMonitor } from './module/karaoke.js'; 
+import { toggleKaraokeDuoSplitMode } from './module/karaoke.js';
 
 
 export function $(id) {
