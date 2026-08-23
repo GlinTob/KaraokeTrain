3c997683e2a3722718d785803ff8eb723ea815bf
GlinTob <jodatomx@hotmail.com>
2026-08-23 15:30:25 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- modules/karaoke.js
+++ modules/karaoke.js
@@ -2,7 +2,7 @@ import { $, safeAdd } from "../script.js";
 import { getLibraryItemsByIdFromSupabase } from "./biblioteca.js";
 import { getAudioController, destroyAudioController, exportStereoWav } from "./audio-controller.js";
 import { startLiveAudio, stopLiveAudio, getLiveAudioState, setMonitoringEnabled } from "./liveAudioService.js";
-import { noteToFrequency, frequencyToMidi } from "./afinador.js";
+import { noteToFrequency, frequencyToMidi, midiToNoteName } from "./afinador.js";
 
 
 let textSegments = [];
