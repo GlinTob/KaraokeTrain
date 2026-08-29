65a35f24b156298216bf78919802eb69d4238ed5
GlinTob <jodatomx@hotmail.com>
2026-08-28 20:26:44 -0500

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- modules/karaoke.js
+++ modules/karaoke.js
@@ -583,7 +583,7 @@ async function loop() {
   }
 }
 
-export function stopKaraokeRecording() {
+function stopKaraokeRecording() {
   if (karaokePitchLoopRafId) {
     cancelAnimationFrame(karaokePitchLoopRafId);
     karaokePitchLoopRafId = null;
@@ -660,7 +660,7 @@ export function stopKaraokeRecording() {
   }
 }
 
-export function restartKaraokeRecording() {
+function restartKaraokeRecording() {
   const track = $("karaokeTrack") || $("karaokeAudio") || $("trackPlayer");
 
   if (track) {
