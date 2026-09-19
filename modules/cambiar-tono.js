31b07762822d88fdc07bee3f76ebe98fa1b332e3
GlinTob <jodatomx@hotmail.com>
2026-09-19 01:29:45 -0500

    Update karaoke module with new imports and fixes

--- modules/cambiar-tono.js
+++ modules/cambiar-tono.js
@@ -1,6 +1,6 @@
 import { $ } from "./utils.js";
 import { getLibraryItemsByTypeFromSupabase, getLibraryItemsByIdFromSupabase, renderLibrary } from "./biblioteca.js?v=4";
-import { loadKaraokeSong } from "./karaoke.js?v=7";
+import { loadKaraokeSong } from "./karaoke.js?v=10";
 import { loadPitchShifterProcessor } from "./worklets.js?v=5";
 
 /**
