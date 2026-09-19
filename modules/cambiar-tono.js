e5370398bab8861e07d7876d99abc9db075d574b
GlinTob <jodatomx@hotmail.com>
2026-09-18 20:13:00 -0500

    Update cambiar-tono.js for improved functionality

--- modules/cambiar-tono.js
+++ modules/cambiar-tono.js
@@ -1,6 +1,6 @@
 import { $ } from "./utils.js";
 import { getLibraryItemsByTypeFromSupabase, getLibraryItemsByIdFromSupabase, renderLibrary } from "./biblioteca.js";
-import { loadKaraokeSong } from "./karaoke.js?v=6";
+import { loadKaraokeSong } from "./karaoke.js?v=7";
 import { loadPitchShifterProcessor } from "./worklets.js";
 
 /**
