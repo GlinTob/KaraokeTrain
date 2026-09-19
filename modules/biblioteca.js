0c3a46db41638251c663cfeffdea1d92ea6f934a
GlinTob <jodatomx@hotmail.com>
2026-09-19 13:36:42 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- modules/biblioteca.js
+++ modules/biblioteca.js
@@ -677,7 +677,7 @@ export async function enviarAlMonitorKaraoke(karaokeItem) {
         track.dataset.karaokeId = String(karaokeItem.id);
         track.load();
     
-        const { setKaraokeData } = await import("./karaoke.js?v=12");
+        const { setKaraokeData } = await import("./karaoke.js?v=13");
         setKaraokeData(
             karaokeItem.transcription || [],
             karaokeItem.name,
